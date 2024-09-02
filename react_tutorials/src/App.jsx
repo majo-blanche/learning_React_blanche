fc15c3d9553859a024057e5cdb14281f7f6ffc26
majo-blanche <blanchemajo24@gmail.com>
2024-09-02 13:54:53 +0100

    react tutorial

--- react_tutorials/src/App.jsx
+++ react_tutorials/src/App.jsx
@@ -3,20 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const Person = () => {
+const Person = (props) => {
     return (
     <>
-        <h1>name:Majo</h1>
-        <h2> last name:blanche</h2>
-        <h2>age:17</h2>
+        <h1>name:{props.name}</h1>
+        <h2> last name:{ props.lastname}</h2>
+        <h2>age:{ props.age}</h2>
     </>
   ) 
   }
-
 function App() {
+  const [counter,setCounter] = useState(0);
   return (
     <div className="App">
-      <Person />
+      <Person name="Majo" lastname="blanche" age="17"/>
+      <Person name="Nekam" lastname="Aurore" age="20"/>
+      <button onClick={()=>setCounter((minCount)=>minCount-1)}>-</button>
+      <h1>{ counter}</h1>
+      <button onClick={()=>setCounter((maxCount)=>maxCount+1)}>+</button>
+       
     </div>
     
   );
